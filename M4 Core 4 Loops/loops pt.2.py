f1880a65e9f7ea062d0b0fe51d67c1e219106dbf
Sierra-Awesome <57376954+Sierra-Awesome@users.noreply.github.com>
2020-01-14 17:31:32 -0500

    I made a change.
    This is just Meeting 4: Loops. So that's the end of that.

---
+++ M4 Core 4 Loops/loops pt.2.py
@@ -0,0 +1,18 @@
+Python 3.8.0 (tags/v3.8.0:fa919fd, Oct 14 2019, 19:21:23) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=================================== RESTART: C:/Users/purpl/Desktop/GWC/loops.py ===================================
+2
+4
+6
+>>> 
+=================================== RESTART: C:/Users/purpl/Desktop/GWC/loops.py ===================================
+2
+4
+6
+>>> 
+=================================== RESTART: C:/Users/purpl/Desktop/GWC/loops.py ===================================
+2
+4
+6
+>>> 
---
+++ M4 Core 4 Loops/loops.py
@@ -0,0 +1,8 @@
+####for i in [1,2,3]:
+####    print(2*i)
+##for x in range(1,4):
+##    print(2*x)
+n = 1
+while (n < 4):
+    print(2*n)
+    n = n + 1
